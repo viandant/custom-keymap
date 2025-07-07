2260f08090f8e239c77f93cd61296d3c4af0fb4a
Viandant <viandant@langenst.de>
2025-07-07 17:31:10 +0200

    Added GPL note to source code file. + some minor editorial changes.

--- user-keymap.el
+++ user-keymap.el
@@ -5,6 +5,20 @@
 ;; Keywords: internal keymap keyboard customization
 ;; URL: https://github.com/viandant/user-keymap
 
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
@@ -14,7 +28,7 @@
 ;; mode USER-KEYMAP-MODE.
 ;;
 ;; Some auxiliary commands are provided:
-;; USER-KEYMAP-SORT-ON-COMMANDS: sort USER-KEYMAP-LIST on commanda
+;; USER-KEYMAP-SORT-ON-COMMANDS: sort USER-KEYMAP-LIST on commands
 ;; USER-KEYMAP-SORT-ON-KEYSEQUENCES: sort USER-KEYMAP-LIST on keysequences
 ;; USER-KEYMAP-ADD: Interactively add an entry to USER-KEYMAP-LIST
 ;; USER-KEYMAP-DELETE: Interactively add an entry to USER-KEYMAP-LIST
@@ -118,8 +132,7 @@ and the customisation dialogue will look nice again."
   "Read a key sequence without echo keystroke help."
   (let ((echo-keystrokes-help nil)
 	(cursor-in-echo-area t)
-	(result nil)
-	)
+	(result nil))
     (while (null result)
       (let*
 	  ((keyseq (read-key-sequence "Keysequence to bind to (C-g to quit): " nil t))
@@ -130,8 +143,7 @@ and the customisation dialogue will look nice again."
 		(if existing-binding
 		    (if (y-or-n-p (format "%s already runs the command %S. Overwrite old setting?" (key-description keyseq) existing-binding))
 			keyseq nil)
-		  keyseq)))
-	))
+		  keyseq)))))
     result))
 
 ;; (defun user-keymap-read-function-symbol--interactive (funsym)
